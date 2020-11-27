7214e6e80eb694d0b482390ea4b06e0ccadf9457
Manish Raj <rajmanish6851@gmail.com>
2020-11-28 01:09:27 +0530

    feat: added the feature of displaying the totalexpenses on the top

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 //Listen to click event
 const element = document.querySelector("#btnExpense");
 
+//get the heading element
+const headingl = document.querySelector("#headingTotal");
+
 //init value  of expense at 0
 let toatalExpense = 0;
+
+//set the heading element to totalExpense
+headingl.textContent = toatalExpense;
 
 // onButtonClick add inputAmount to TotalExpense
     function AddExpenseToTotal(){
         //read value from inputAmount
         const inputElement = document.querySelector("#inputAmount");
         const textAmount = inputElement.value;
-        console.log({textAmount});
+
         //convert it to number
         const expense = parseInt(textAmount, 10);
-        console.log({expense});
+
         //add that value to toatalExpense
+        toatalExpense = toatalExpense + expense;
+
+        //set the heading element to totalExpense
+        headingl.textContent = toatalExpense;
     }
 
 //Listen to click event
